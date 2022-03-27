d93863fd4c6f861ff2c792ea9487a78a8eccaba9
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2022-03-27 09:01:10 +0530

    python string

---
+++ app.py
@@ -0,0 +1,20 @@
+# name="Abhishek"
+
+# print("my name is "+name+" ")
+# name = "ranjan"
+# print("my name is "+name+" ")
+
+# print("hello world")
+# print("    /|")
+# print("   / |")
+# print("  /  |")
+# print(" /___|")
+
+variable = "hello world"
+print(variable.upper())
+print(variable.isupper())
+print(variable.upper().isupper())
+
+#find the length of string
+
+print(len(variable))
